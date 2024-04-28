133668cdea4e1a962d4d9ac3730dde89fbd9f8e2
Abdul-Majid Aladejana <aladejanamajid48@gmail.com>
2024-04-28 12:34:51 +0100

    add Partner padding

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -4,7 +4,7 @@ function Partners(props) {
       <div className="m-auto text-center text-3xl font-bold mt-8 sm:-mt-10 sm:mb-5 mb-8 sm:w-5/12 w-3/4 sm:m-auto">
         Working with world class partners. We`re partner first!
       </div>
-      <div className="h-24 m-auto w-auto bg-black flex flex-row  sm:gap-8 sm:px-32 px-20 gap-2 items-center">
+      <div className="h-24 m-auto w-auto bg-black flex flex-row  sm:gap-8 sm:px-32 px-5 gap-2 items-center">
         {sponsors.map((item, index) => {
           return <Sponsors sponsor={item.pic} />;
         })}
